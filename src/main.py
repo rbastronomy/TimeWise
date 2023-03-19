3497b1b220b0ba70f0da726c534e897926c06e4b
Luciano Martinez <lucianoemartinezl@gmail.com>
2023-03-19 16:09:01 -0300

    prueba flet

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+"""
 print("Hola Mundo")
 
 class Animal:
@@ -18,3 +19,28 @@ Animalia = Animal("Gato", 4)
 Animalia.Sonido()
 Gatito = Gato("Gato", 4)
 Gatito.Sonido()
+"""
+import flet as ft
+def main(page: ft.Page):
+    page.title = "Ejemplo Fleto"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    txt_number = ft.TextField(value="0", text_align= ft.TextAlign.RIGHT, width = 100)
+
+    def minus_click(e):
+        txt_number.value = str(int(txt_number.value) - 1)
+        page.update()
+    def plus_click(e):
+        txt_number.value = str(int(txt_number.value) + 1)
+        page.update()
+    page.add(
+        ft.Row(
+            [
+                ft.IconButton(ft.icons.REMOVE, on_click = minus_click),
+                txt_number,
+                ft.IconButton(ft.icons.ADD, on_click = plus_click),
+            ],
+            alignment = ft.MainAxisAlignment.CENTER,
+        )
+    )
+#ft.app(target = main)
+ft.app(target = main, view = ft.WEB_BROWSER)
