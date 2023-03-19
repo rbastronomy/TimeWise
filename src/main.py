c76a5ef7d1c5fd38ed9ce0a2b610092bf1377f36
Luciano Martinez <lucianoemartinezl@gmail.com>
2023-03-19 15:38:12 -0300

    OOP aplicado

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
 print("Hola Mundo")
+
+class Animal:
+    def __init__(self, especie, peso):
+        self.especie = especie
+        self.peso = peso
+
+    def Sonido(self):
+        print("A")
+class Gato(Animal):
+    def __init__(self, especie, peso):
+        super().__init__(especie, peso)
+    def Sonido(self):
+        print("Meow")
+
+
+Animalia = Animal("Gato", 4) 
+Animalia.Sonido()
+Gatito = Gato("Gato", 4)
+Gatito.Sonido()
